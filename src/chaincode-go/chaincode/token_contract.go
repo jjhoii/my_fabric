12418b647fb845ad1b40f21680335ca75ac6e26c
정지환 <kkiu1756@gmail.com>
2021-04-29 15:16:11 +0900

    chore: SetBalance 리시버 적용

--- src/chaincode-go/chaincode/token_contract.go
+++ src/chaincode-go/chaincode/token_contract.go
@@ -246,7 +246,7 @@ func SetTransaction(ctx contractapi.TransactionContextInterface, from string, to
 	return &transaction, err
 }
 
-func SetBalance(ctx contractapi.TransactionContextInterface, id string, balance int) (*User, error) {
+func (s *SmartContract) SetBalance(ctx contractapi.TransactionContextInterface, id string, balance int) (*User, error) {
 	user, err := GetUser(ctx, id)
 	if err != nil {
 		return nil, fmt.Errorf("user id %s does not exist", id)
